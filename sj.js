be415d588718ab69533e7c1036629b5847a059e0
ta222222 <125400362+ta222222@users.noreply.github.com>
2025-04-18 06:34:14 +0100

    Update sj.js
    error on video load smh

--- sj.js
+++ sj.js
@@ -1,47 +1,47 @@
-        const body = document.body;
-        const imageContainer = document.querySelector('.image-container');
-        const videoWrapper = document.getElementById('videoWrapper');
-        let player;
+const body = document.body;
+const imageContainer = document.querySelector('.image-container');
+const videoWrapper = document.getElementById('videoWrapper');
+let player;
 
-        function initializePlayer() {
-            const tag = document.createElement('script');
-            tag.src = "https://www.youtube.com/iframe_api"; 
-            const firstScriptTag = document.getElementsByTagName('script')[0];
-            firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+function initializePlayer() {
+    const tag = document.createElement('script');
+    tag.src = "https://www.youtube.com/iframe_api"; 
+    const firstScriptTag = document.getElementsByTagName('script')[0];
+    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+}
+
+function onYouTubeIframeAPIReady() {
+    player = new YT.Player('player', {
+        height: '100%',
+        width: '100%',
+        videoId: '-R38JqMHK8A',
+        playerVars: {
+            'autoplay': 1,
+            'controls': 1,
+            'modestbranding': 1,
+            'rel': 0,
+            'showinfo': 0,
+            'disablekb': 0,
+            'iv_load_policy': 3
         }
+    });
+}
 
-        function onYouTubeIframeAPIReady() {
-            player = new YT.Player('player', {
-                height: '100%',
-                width: '100%',
-                videoId: '-R38JqMHK8A',
-                playerVars: {
-                    'autoplay': 1,
-                    'controls': 1,
-                    'modestbranding': 1,
-                    'rel': 0,
-                    'showinfo': 0,
-                    'disablekb': 0,
-                    'iv_load_policy': 3
-                }
-            });
-        }
+setTimeout(() => {
+    body.style.backgroundColor = 'black';
+    imageContainer.style.opacity = '1';
+}, 100);
 
-        setTimeout(() => {
-            body.style.backgroundColor = 'black';
-            imageContainer.style.opacity = '1';
-        }, 100);
+setTimeout(() => {
+    imageContainer.style.opacity = '0';
+    setTimeout(() => {
+        imageContainer.style.display = 'none';
+        videoWrapper.style.display = 'flex';
+    }, 1000);
+}, 3000);
 
-        setTimeout(() => {
-            imageContainer.style.opacity = '0';
-            setTimeout(() => {
-                    imageContainer.style.display = 'none';
-                    videoWrapper.style.display = 'flex';
-            }, 1000);
-        }, 3000);
-
-        setTimeout(() => {
-            body.style.backgroundColor = 'red';
-            videoWrapper.style.opacity = '1';
-            initializePlayer();
-        }, 5000);
+setTimeout(() => {
+    body.style.backgroundColor = 'red';
+    videoWrapper.style.opacity = '1';
+    initializePlayer();
+}, 5000);
